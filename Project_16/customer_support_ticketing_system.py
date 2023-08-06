d7b8eb125d0311e26b363b128e59d4a36034d2e0
Moses-main <105107462+Moses-main@users.noreply.github.com>
2023-08-06 16:02:49 +0000

    Updated the customer_ticket_system
    Add more features to the app like listing according to priority, deleting the ticket already added

--- Project_16/customer_support_ticketing_system.py
+++ Project_16/customer_support_ticketing_system.py
@@ -1,59 +1,72 @@
-class Ticket:
+class CustomerSupportTicket:
     def __init__(self, customer_name, issue_description, priority):
         self.customer_name = customer_name
         self.issue_description = issue_description
         self.priority = priority
-
 
 class TicketingSystem:
     def __init__(self):
         self.tickets = []
 
     def create_ticket(self, customer_name, issue_description, priority):
-        # Create a new ticket instance and add it to the list of tickets
-        ticket = Ticket(customer_name, issue_description, priority)
+        ticket = CustomerSupportTicket(customer_name, issue_description, priority)
         self.tickets.append(ticket)
-        print("Ticket created successfully!")
+        self.tickets.sort(key=lambda x: x.priority, reverse=True)
 
     def process_tickets(self):
-        if not self.tickets:
-            print("No tickets to process.")
-            return
-
-        # Sorting tickets based on priority (higher priority first)
-        self.tickets.sort(key=lambda ticket: ticket.priority, reverse=True)
-
-        print("Processing tickets:")
-        for idx, ticket in enumerate(self.tickets, start=1):
-            print(f"Ticket {idx}:")
-            print(f"Customer Name: {ticket.customer_name}")
+        while self.tickets:
+            ticket = self.tickets.pop(0)
+            print(f"Processing ticket from {ticket.customer_name} - Priority: {ticket.priority}")
             print(f"Issue Description: {ticket.issue_description}")
-            print(f"Priority: {ticket.priority}")
-            print("-----------------------")
+            print("Ticket resolved.\n")
 
-    def run(self):
-        while True:
-            print("\nCustomer Support Ticketing System")
-            print("1. Create Ticket")
-            print("2. Process Tickets")
-            print("3. Exit")
-            choice = input("Select an option: ")
+    def delete_ticket(self, index):
+        if 0 <= index < len(self.tickets):
+            deleted_ticket = self.tickets.pop(index)
+            print(f"Ticket from {deleted_ticket.customer_name} with priority {deleted_ticket.priority} deleted.")
+        else:
+            print("Invalid ticket index.")
 
-            if choice == "1":
-                customer_name = input("Enter customer name: ")
-                issue_description = input("Enter issue description: ")
-                priority = int(input("Enter priority (1 - Low, 2 - Medium, 3 - High): "))
-                self.create_ticket(customer_name, issue_description, priority)
-            elif choice == "2":
-                self.process_tickets()
-            elif choice == "3":
-                print("Exiting the ticketing system.")
-                break
-            else:
-                print("Invalid choice. Please select a valid option.")
+    def list_customers_by_priority(self):
+        sorted_tickets = sorted(self.tickets, key=lambda x: x.priority, reverse=True)
+        for index, ticket in enumerate(sorted_tickets):
+            print(f"{index + 1}. Customer: {ticket.customer_name} - Priority: {ticket.priority}")
 
+def main():
+    ticket_system = TicketingSystem()
+
+    while True:
+        print("1. Create a ticket")
+        print("2. Process tickets")
+        print("3. Delete a ticket")
+        print("4. List customers by priority")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            customer_name = input("Enter customer name: ")
+            issue_description = input("Enter issue description: ")
+            priority = int(input("Enter priority (1 - Low, 2 - Medium, 3 - High): "))
+            ticket_system.create_ticket(customer_name, issue_description, priority)
+            print("Ticket created successfully.\n")
+
+        elif choice == '2':
+            ticket_system.process_tickets()
+
+        elif choice == '3':
+            index = int(input("Enter the index of the ticket to delete: "))
+            ticket_system.delete_ticket(index)
+
+        elif choice == '4':
+            ticket_system.list_customers_by_priority()
+
+        elif choice == '5':
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid choice. Please select a valid option.\n")
 
 if __name__ == "__main__":
-    ticketing_system = TicketingSystem()
-    ticketing_system.run()
-                 
+    main()
+      
